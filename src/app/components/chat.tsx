7b6f74e724121138297a1719f48cc16a1524dacb
Judah <judah.boadu@stud.hs-ruhrwest.de>
2024-06-17 19:45:34 +0200

    merged

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Grid } from "@mui/material";
 import BasicModal from "./BasicModal";
 import { QAObject } from "./Questionnaire";
