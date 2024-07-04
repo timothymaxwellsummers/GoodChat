9664144b2a672c491f2cd87007153e3a627cb1b9
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2024-07-04 20:30:22 +0200

    DashboarComp setup

--- src/app/dashboard/components/options.tsx
+++ src/app/dashboard/components/options.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React from 'react';
+import React from "react";
 
 const Options: React.FC = () => {
   return (
-    <div className="">
+    <div className="px-4 pt-14">
+      <div className="text-3xl font-medium">🛋️ Your Dashboard</div>
+      <div className="text-xl text-gray-500 pt-2 pb-2">
+        Here you can start chatting or explore more options.
+      </div>
     </div>
   );
 };
